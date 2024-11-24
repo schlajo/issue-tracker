89b9584e618e15b4092ded4071917fd599a6677c
schlajo <schlajo1981@gmail.com>
2024-11-23 22:37:48 -0600

    Corrected mistake. Replaced "PrismaClientValidationError" with "validation" on line 16 of route.ts file

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 import prisma from "@/prisma/client";
+
+
 const createIssueSchema = z.object({
     title: z.string().min(1).max(255),
     description: z.string().min(1)
@@ -11,7 +13,7 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
     if (!validation.success)
-        return NextResponse.json(PrismaClientValidation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.errors, {status: 400})
 
     const newIssue = await prisma.issue.create({
         data: {title: body.title, description: body.description }
